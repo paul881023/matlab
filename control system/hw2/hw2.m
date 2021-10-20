7e8202b724553f2f34de4a8c4e20beb1da398255
paul881023 <E94076021@gs.ncku.edu.tw>
2021-10-20 15:50:51 +0800

    123

--- control system/hw2/hw2.m
+++ control system/hw2/hw2.m
@@ -1,16 +1,17 @@
+%This is the 2 hw for control system
+%design a system
+%editor: Wang Bo An
+
 clear all;
-k = 4;
-Cs = zpk([],[1],k);
+k = 3;
+Cs = zpk([],[-1-0.3*sqrt(1-(1/0.3)^2)*i,-1+0.1*sqrt(1-(1/0.3)^2)*i],k);
 Gp = zpk([],[-2],1);
 CsGp = series(Cs,Gp);
 sys_continuous = feedback(CsGp,1);
 pole_continuous = pole(sys_continuous);
-tf = 4;
-t = 0:0.01:tf;
+tf =  :0.01:tf;
 input = exp(t);
 output = lsim(sys_continuous,input,t);
-
 plot(t,output,'r');
 hold on
 plot(t,input,'b');
-xlabel('Time (sec)');ylabel('data');
