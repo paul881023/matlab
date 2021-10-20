4d19886a7b7703b6fa51af3eda44ec32e11ddda9
paul881023 <E94076021@gs.ncku.edu.tw>
2021-10-20 23:25:16 +0800

    123

--- control system/hw2/hw2.m
+++ control system/hw2/hw2.m
@@ -3,15 +3,17 @@
 %editor: Wang Bo An
 
 clear all;
-k = 3;
-Cs = zpk([],[-1-0.3*sqrt(1-(1/0.3)^2)*i,-1+0.1*sqrt(1-(1/0.3)^2)*i],k);
+k = 4;
+Cs = zpk([],[1],k);
 Gp = zpk([],[-2],1);
 CsGp = series(Cs,Gp);
 sys_continuous = feedback(CsGp,1);
 pole_continuous = pole(sys_continuous);
-tf =  :0.01:tf;
+tf = 4;
+t = 0:0.01:tf;
 input = exp(t);
 output = lsim(sys_continuous,input,t);
 plot(t,output,'r');
 hold on
 plot(t,input,'b');
+xlabel('time');ylabel('data')
